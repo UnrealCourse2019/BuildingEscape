0d3339f1cc1eefe15c8fd37515a0c431735520ba
Michael Bridges <mike@michaelbridges.co.uk>
2019-11-19 16:46:53 +0000

    4 Setting Up The Building Escape Project

---
+++ Building_Escape/Source/Building_Escape/Building_Escape.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Building_Escape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Building_Escape, "Building_Escape" );
---
+++ Building_Escape/Source/Building_Escape/Building_EscapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Building_EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_ESCAPE_API ABuilding_EscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
