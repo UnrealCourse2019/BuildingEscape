bfcae70dcb38757f9e9f0f73983c769600689d0d
Michael Bridges <mike@michaelbridges.co.uk>
2019-11-25 19:11:05 +0000

    8 Logging To The Output Log

--- Building_Escape/Source/Building_Escape/WorldPosition.cpp
+++ Building_Escape/Source/Building_Escape/WorldPosition.cpp
@@ -20,7 +20,9 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	UE_LOG(LogTemp, Display, TEXT("Just a log, nothing to see!"));
+	UE_LOG(LogTemp, Warning, TEXT("This is a Warning!"));
+	UE_LOG(LogTemp, Error, TEXT("ERROR!"))
 	
 }
 
