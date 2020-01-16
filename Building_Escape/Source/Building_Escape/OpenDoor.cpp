51b668294c11b36b1555e180a6506cdfd723e97f
Michael Bridges <mike@michaelbridges.co.uk>
2020-01-16 12:32:03 +0000

    26 Protecting From A Null Pointer
    has the open door component on it, but no pressureplate set

--- Building_Escape/Source/Building_Escape/OpenDoor.cpp
+++ Building_Escape/Source/Building_Escape/OpenDoor.cpp
@@ -23,6 +23,11 @@ void UOpenDoor::BeginPlay()
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
 	TargetYaw += InitialYaw; //TargetYaw = TargetYaw + InitialYaw;
+
+	if(!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door component on it, but no pressureplate set!"), *GetOwner()->GetName());	
+	}
 }
 
 
@@ -31,7 +36,7 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	
-	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens))
 	{
 		OpenDoor(DeltaTime);
 	}	
