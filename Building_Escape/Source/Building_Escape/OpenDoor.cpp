a06be103316bf9842f3e2e195da25573b5b68e83
Michael Bridges <mike@michaelbridges.co.uk>
2020-01-16 12:39:49 +0000

    27 Get Player To open Door

--- Building_Escape/Source/Building_Escape/OpenDoor.cpp
+++ Building_Escape/Source/Building_Escape/OpenDoor.cpp
@@ -3,6 +3,8 @@
 
 #include "OpenDoor.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
 
 // Sets default values for this component's properties
@@ -28,6 +30,8 @@ void UOpenDoor::BeginPlay()
 	{
 		UE_LOG(LogTemp, Error, TEXT("%s has the open door component on it, but no pressureplate set!"), *GetOwner()->GetName());	
 	}
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 
